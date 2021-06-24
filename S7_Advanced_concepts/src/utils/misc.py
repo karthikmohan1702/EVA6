5e6a2514689a9e89a81686ede7ed535ede6ae013
karthikmohan1702 <47082769+karthikmohan1702@users.noreply.github.com>
2021-06-25 01:40:18 +0530

    Added mean_std & other func

--- S7_Advanced_concepts/src/utils/misc.py
+++ S7_Advanced_concepts/src/utils/misc.py
@@ -3,6 +3,9 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
+from collections import Counter
+from collections import OrderedDict
 
 
 def get_device_info():
@@ -11,10 +14,57 @@ def get_device_info():
     print(device)
     return device
 
+
 def get_model_summary(model, input_size):
     device = get_device_info()
     model_to_device = model.to(device)
     return summary(model, input_size)
 
 
+def get_mean_std(loader):
+    channels_sum, channels_sqrd_sum, num_batches = 0, 0, 0
 
+    for data, _ in tqdm(loader):
+        channels_sum += torch.mean(data, dim=[0, 2, 3])
+        channels_sqrd_sum += torch.mean(data ** 2, dim=[0, 2, 3])
+        num_batches += 1
+
+    mean = channels_sum / num_batches
+    std = (channels_sqrd_sum / num_batches - mean ** 2) ** 0.5
+
+    return mean, std
+
+
+def get_class_dist(classes, dataset):
+  ord_dict = OrderedDict()
+  for id, cl in enumerate(classes):
+    ord_dict[id] = cl  
+
+  count_label = []
+  count_img_size = []
+  for img, label in dataset:
+    label = ord_dict[label]
+    count_label.append(label)
+    count_img_size.append(img.numpy().shape)
+  return Counter(count_label), Counter(count_img_size)
+
+def print_dist_stats(train_set, test_set):
+  print("Distribution of classes in Train Dataset")
+  print("=="*30)
+  display(get_class_dist(train_set.classes, train_set)[0])
+  print("\n")
+
+  print("Distribution of classes in Test Dataset")
+  print("=="*30)
+  display(get_class_dist(test_set.classes, test_set)[0])
+  print("\n")
+
+  print("Distribution of size of image in Train Dataset")
+  print("=="*30)
+  display(get_class_dist(train_set.classes, train_set)[1])
+  print("\n")
+
+  print("Distribution of size of image in Test Dataset")
+  print("=="*30)
+  display(get_class_dist(test_set.classes, test_set)[1])
+  print("\n")
